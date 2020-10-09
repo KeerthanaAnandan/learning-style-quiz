46ec0dc92eb3902674c7ee93594baa333543546c
keerthanaAnandan <keerthanaanandanak@gmail.com>
2020-10-09 23:00:21 +0530

    updated

--- dist/js/site.js
+++ dist/js/site.js
@@ -1,205 +1,73 @@
-const progressText = document.getElementById("progressText");
-const scoreText = document.getElementById("score");
-const progressBarFull = document.getElementById("progressBarFull");
-
 let questions = [
   {
     id: 1,
-    question: "I enjoy participating in classroom discussions and debates.",
+    question: "How often do you think of your ex?",
     answer: "Random Access Memory",
-    options: ["A lot like me", "Somewhat like me", "Not like me"],
+    options: ["Never", "Once a week", "Every day", "Multiple times a day"],
   },
   {
     id: 2,
-    question: "I try to remember information by picturing it in my mind.",
+    question: "Do you blame yourself?",
     answer: "Central Processing Unit",
-    options: ["A lot like me", "Somewhat like me", "Not like me"],
+    options: ["No", "Somewhat", "Yes", "Absolutely"],
   },
   {
     id: 3,
-    question: "My best school subjects are English and foreign languages",
-    answer: "Central Processing Unit",
-    options: ["A lot like me", "Somewhat like me", "Not like me"],
+    question: "How would you describe the way you feel?",
+    answer: "Electronic Mail",
+    options: [
+      "I’ve moved on",
+      "I feel somewhat sad but I’ll get over it",
+      "I’m hurting",
+      "The pain is unbearable",
+    ],
   },
   {
     id: 4,
-    question: "I sound words out when I am trying to learn to spell them.",
-    answer: "Central Processing Unit",
-    options: ["A lot like me", "Somewhat like me", "Not like me"],
+    question: "What was the cause for the breakup?",
+    answer: "Electronic Mail",
+    options: ["Cheating", "Abuse", "Lack of communication", "Something else"],
   },
   {
     id: 5,
-    question:
-      "I draw the letters of a word with my fingers when trying to learn its spelling.",
-    answer: "Central Processing Unit",
-    options: ["A lot like me", "Somewhat like me", "Not like me"],
+    question: "Are you ready to date again?",
+    answer: "Electronic Mail",
+    options: ["Absolutely", "In the future", "No time soon", "Never again"],
   },
   {
     id: 6,
-    question: "It helps me to read out loud when studying from my textbooks.",
-    answer: "Central Processing Unit",
-    options: ["A lot like me", "Somewhat like me", "Not like me"],
+    question: "Who broke up with who?",
+    answer: "Electronic Mail",
+    options: [
+      "He broke up with you",
+      "You broke up with him",
+      "It was mutual",
+      "You still don’t know",
+    ],
   },
   {
     id: 7,
-    question: "I create songs and jingles to remember information.",
-    answer: "Central Processing Unit",
-    options: ["A lot like me", "Somewhat like me", "Not like me"],
+    question: "Given the chance would you get back with you ex?",
+    answer: "Electronic Mail",
+    options: ["Absolutely not", "Maybe", "Yeah", "In a minute"],
   },
   {
     id: 8,
-    question: "I do best in subjects such as math and science.",
-    answer: "Central Processing Unit",
-    options: ["A lot like me", "Somewhat like me", "Not like me"],
-  },
-  {
-    id: 9,
-    question: "I follow oral directions better than written directions.",
-    answer: "Central Processing Unit",
-    options: ["A lot like me", "Somewhat like me", "Not like me"],
-  },
-  {
-    id: 10,
     question:
-      "I learn how objects work by taking them apart and putting them back together.",
-    answer: "Central Processing Unit",
-    options: ["A lot like me", "Somewhat like me", "Not like me"],
-  },
-  {
-    id: 11,
-    question: "I tap my foot or a pencil while thinking",
-    answer: "Central Processing Unit",
-    options: ["A lot like me", "Somewhat like me", "Not like me"],
-  },
-  {
-    id: 12,
-
-    question: "I do well in lab classes.",
-    answer: "Central Processing Unit",
-    options: ["A lot like me", "Somewhat like me", "Not like me"],
-  },
-  {
-    id: 13,
-    question:
-      "I learn a lot about a person by observing their facial expressions and body language.",
-    answer: "Central Processing Unit",
-    options: ["A lot like me", "Somewhat like me", "Not like me"],
-  },
-  {
-    id: 14,
-    question: "I am good at drawing things.",
-    answer: "Central Processing Unit",
-    options: ["A lot like me", "Somewhat like me", "Not like me"],
-  },
-  {
-    id: 15,
-    question:
-      "I need to participate in an activity in order to learn how to do it.",
-    answer: "Central Processing Unit",
-    options: ["A lot like me", "Somewhat like me", "Not like me"],
-  },
-  {
-    id: 16,
-    question: "I learn a lot from diagrams and illustrations in textbooks.",
-    answer: "Central Processing Unit",
-    options: ["A lot like me", "Somewhat like me", "Not like me"],
-  },
-  {
-    id: 17,
-    question: "I am good at using my hands to fix things.",
-    answer: "Central Processing Unit",
-    options: ["A lot like me", "Somewhat like me", "Not like me"],
-  },
-  {
-    id: 18,
-    question:
-      "I need explanations of diagrams, graphs, and maps in order to understand them",
-    answer: "Central Processing Unit",
-    options: ["A lot like me", "Somewhat like me", "Not like me"],
-  },
-  {
-    id: 19,
-    question: "I am good at acting out stories.",
-    answer: "Central Processing Unit",
-    options: ["A lot like me", "Somewhat like me", "Not like me"],
-  },
-  {
-    id: 20,
-    question: "I can tell if a word is spelled correctly by looking at it",
-    answer: "Central Processing Unit",
-    options: ["A lot like me", "Somewhat like me", "Not like me"],
-  },
-  {
-    id: 21,
-    question: "I use my hands when explaining something to another person.",
-    answer: "Central Processing Unit",
-    options: ["A lot like me", "Somewhat like me", "Not like me"],
-  },
-  {
-    id: 22,
-    question: "I am good at doing word search puzzles",
-    answer: "Central Processing Unit",
-    options: ["A lot like me", "Somewhat like me", "Not like me"],
-  },
-  {
-    id: 23,
-    question: "I prefer teachers who write information on the chalkboard.",
-    answer: "Central Processing Unit",
-    options: ["A lot like me", "Somewhat like me", "Not like me"],
-  },
-  {
-    id: 24,
-    question: " I prefer teachers who spend a lot of time explaining things.",
-    answer: "Central Processing Unit",
-    options: ["A lot like me", "Somewhat like me", "Not like me"],
-  },
-  {
-    id: 25,
-    question:
-      "When I take a test, I can see the answer in my head the way it appeared in my notes or textbook",
-    answer: "Central Processing Unit",
-    options: ["A lot like me", "Somewhat like me", "Not like me"],
-  },
-  {
-    id: 26,
-    question: "I use pictures, arrows, and other symbols when I take notes.",
-    answer: "Central Processing Unit",
-    options: ["A lot like me", "Somewhat like me", "Not like me"],
-  },
-  {
-    id: 27,
-    question:
-      "When learning to spell a word, I trace the letters with my finger",
-    answer: "Central Processing Unit",
-    options: ["A lot like me", "Somewhat like me", "Not like me"],
-  },
-  {
-    id: 28,
-    question:
-      "I learn better by listening to a lecture than by reading a textbook.",
-    answer: "Central Processing Unit",
-    options: ["A lot like me", "Somewhat like me", "Not like me"],
-  },
-  {
-    id: 29,
-    question: "I remember things I hear better than things I see.",
-    answer: "Central Processing Unit",
-    options: ["A lot like me", "Somewhat like me", "Not like me"],
-  },
-  {
-    id: 30,
-    question: "I am good at designing graphs, charts, and diagrams.",
-    answer: "Central Processing Unit",
-    options: ["A lot like me", "Somewhat like me", "Not like me"],
+      "You find an expensive piece of jewelry that belongs to your ex you ?",
+    answer: "Electronic Mail",
+    options: [
+      "Donate it",
+      "Give it back to them",
+      "Keep it to remember them",
+      "Break it into a million pieces",
+    ],
   },
 ];
 
 let question_count = 0;
 let points = 0;
-let apoints = 0;
-let vpoints = 0;
-let tpoints = 0;
-let Max_Questions = 30;
+let Max_Questions = 8;
 
 window.onload = function () {
   show(question_count);
@@ -217,33 +85,65 @@ function next() {
 
   let user_answer = document.querySelector("li.option.active").innerHTML;
   // check if the answer is right or wrong
-  if (user_answer == questions[question_count].answer) {
-    points += 10;
-    sessionStorage.setItem("points", points);
-  } else if (user_answer == "A lot like me") {
-    console.log("Audi");
-    apoints += 1;
-    sessionStorage.setItem("apoints", apoints);
-  } else if (user_answer == "Somewhat like me") {
-    vpoints += 1;
-    sessionStorage.setItem("vpoints", vpoints);
-  } else if (user_answer == "Not like me") {
-    tpoints += 1;
-
-    sessionStorage.setItem("tpoints", tpoints);
+  //if (user_answer == questions[question_count].answer) {
+  if (
+    user_answer === "Never" ||
+    user_answer === "No" ||
+    user_answer === "I’ve moved on" ||
+    user_answer === "Never again" ||
+    user_answer === "You broke up with him" ||
+    user_answer === "Absolutely not"
+  ) {
+    points += 5;
+  } else if (
+    user_answer === "Once a week" ||
+    user_answer === "Somewhat" ||
+    user_answer === "I feel somewhat sad but I’ll get over it" ||
+    user_answer === "No time soon" ||
+    user_answer === "It was mutual" ||
+    user_answer === "Give it back to them"
+  ) {
+    points += 4;
+  } else if (user_answer === "Donate it" || user_answer === "Maybe") {
+    points += 3;
+  } else if (
+    user_answer === "Every day" ||
+    user_answer === "Yes" ||
+    user_answer === "I’m hurting" ||
+    user_answer === "In the future" ||
+    user_answer === "He broke up with you" ||
+    user_answer === "You still don’t know" ||
+    user_answer === "Break it into a million pieces" ||
+    user_answer === "Yeah"
+  ) {
+    points += 2;
+  } else if (
+    user_answer === "Multiple times a day" ||
+    user_answer === "Absolutely" ||
+    user_answer === "The pain is unbearable" ||
+    user_answer === "Cheating" ||
+    user_answer === "Abuse" ||
+    user_answer === "Lack of communication" ||
+    user_answer === "Something else" ||
+    user_answer === "Keep it to remember them" ||
+    user_answer === "In a minute"
+  ) {
+    points += 1;
   }
-  //console.log(points);
+  sessionStorage.setItem("points", points);
+
+  console.log(points);
 
   question_count++;
   show(question_count);
   toggleActive();
-
   //Update the progress Bar
 
   const progressBarFull = document.getElementById("progressBarFull");
 
   progressBarFull.style.width = ` ${(question_count / Max_Questions) * 100}%`;
 }
+
 function show(count) {
   //progress text
   const progressText = document.getElementById("progressText");
@@ -251,7 +151,7 @@ function show(count) {
   progressText.innerText = `Question ${count + 1}/${Max_Questions}`;
 
   let question = document.getElementById("questions");
-  let [first, second, third] = questions[count].options;
+  let [first, second, third, fourth] = questions[count].options;
 
   question.innerHTML = `
   <h2>Q${count + 1}. ${questions[count].question}</h2>
@@ -259,7 +159,7 @@ function show(count) {
   <li class="option">${first}</li>
   <li class="option">${second}</li>
   <li class="option">${third}</li>
-
+  <li class="option">${fourth}</li>
 </ul> 
   `;
   toggleActive();
@@ -278,3 +178,5 @@ function toggleActive() {
     };
   }
 }
+let usertimehide = document.querySelector(".quiz_timer");
+usertimehide.style.display = "none";
--- dist/js/userInfo.js
+++ dist/js/userInfo.js
@@ -1,49 +1,37 @@
 let user_name = sessionStorage.getItem("name");
 let user_points = sessionStorage.getItem("points");
-let user_apoints = sessionStorage.getItem("apoints");
-let user_vpoints = sessionStorage.getItem("vpoints");
-let user_tpoints = sessionStorage.getItem("tpoints");
 let user_time = sessionStorage.getItem("time");
-let audi = document.querySelector(".audi");
-let visco = document.querySelector(".vis");
-let taco = document.querySelector(".tac");
-let typeshere = document.querySelector("span.typehere");
-let audiexp = document.querySelector(".audiexp");
-let viscoexp = document.querySelector(".viscoexp");
-let tacoexp = document.querySelector(".tacoexp");
-//////coding logic
-let apo = Math.round(user_apoints * 3.33);
-let vpo = Math.round(user_vpoints * 3.33);
-let tpo = Math.round(user_tpoints * 3.33);
-
+document.querySelector("span.name").innerHTML = user_name;
 document.querySelector("span.points").innerHTML = user_points;
-document.querySelector("span.apoints").innerHTML = apo + "%";
-document.querySelector("span.vpoints").innerHTML = vpo + "%";
-document.querySelector("span.tpoints").innerHTML = tpo + "%";
-console.log("end page");
-console.log(user_points);
-console.log(user_apoints);
-console.log(user_vpoints);
-console.log(user_tpoints);
-audiexp.style.display = "none";
-viscoexp.style.display = "none";
-tacoexp.style.display = "none";
+document.querySelector("span.time_taken").innerHTML = user_time;
+let usertimehide = document.querySelector(".usertime");
+let usernamehide = document.querySelector(".username");
+let userpointhide = document.querySelector(".userpoints");
+let high = document.querySelector(".high");
+let medium = document.querySelector(".medium");
+let low = document.querySelector(".low");
+let belowlow = document.querySelector(".belowlow");
+sessionStorage.clear();
 
-if (apo > vpo && apo > tpo) {
-  typeshere.innerHTML = "Auditory";
-  //console.group("audi");
-  audiexp.style.display = "block";
-  //document.querySelector(".wordshere").innerHTML = "bitch you are audi";
-} else if (vpo > apo && vpo > tpo) {
-  typeshere.innerHTML = "Visual";
-  //console.group("visu");
-  viscoexp.style.display = "block";
-  // document.querySelector(".wordshere").innerHTML = "bitch you are v";
-} else if (tpo > apo && tpo > vpo) {
-  typeshere.innerHTML = "Tactile/Kinesthetic";
-  //console.group("tac");
-  tacoexp.style.display = "block";
-  // document.querySelector(".wordshere").innerHTML = "bitch you are tacos";
+high.style.display = "none";
+medium.style.display = "none";
+low.style.display = "none";
+belowlow.style.display = "none";
+
+if (user_points >= 29 && user_points <= 35) {
+  console.log(" 29-35");
+  high.style.display = "block";
+} else if (user_points >= 15 && user_points <= 27) {
+  console.log(" 15-27");
+  medium.style.display = "block";
+} else if (user_points >= 10 && user_points <= 14) {
+  console.log(" 10-14");
+  low.style.display = "block";
+} else if (user_points <= 9) {
+  console.log(" you are 9 and below");
+  belowlow.style.display = "block";
 }
 
-sessionStorage.clear();
+usertimehide.style.display = "none";
+usernamehide.style.display = "none";
+userpointhide.style.display = "none";
