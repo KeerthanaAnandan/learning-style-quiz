655091e1c14bd6afd9faabb511c3e36622b004f0
keerthanaAnandan <keerthanaanandanak@gmail.com>
2020-10-20 13:54:54 +0530

    final

--- dist/js/userInfo.js
+++ dist/js/userInfo.js
@@ -4,13 +4,13 @@ let user_apoints = sessionStorage.getItem("apoints");
 let user_vpoints = sessionStorage.getItem("vpoints");
 let user_tpoints = sessionStorage.getItem("tpoints");
 let user_time = sessionStorage.getItem("time");
-let audi = document.querySelector(".audi");
-let visco = document.querySelector(".vis");
-let taco = document.querySelector(".tac");
+//let audi = document.querySelector(".audi");
+//let visco = document.querySelector(".vis");
+//let taco = document.querySelector(".tac");
 let typeshere = document.querySelector("span.typehere");
-let audiexp = document.querySelector(".audiexp");
-let viscoexp = document.querySelector(".viscoexp");
-let tacoexp = document.querySelector(".tacoexp");
+//let audiexp = document.querySelector(".audiexp");
+//let viscoexp = document.querySelector(".viscoexp");
+//let tacoexp = document.querySelector(".tacoexp");
 ///hide timer
 document.querySelector(".usertime").style.display = "none";
 //////coding logicr
@@ -30,36 +30,36 @@ console.log(user_points);
 console.log(user_apoints);
 console.log(user_vpoints);
 console.log(user_tpoints);
-audiexp.style.display = "none";
-viscoexp.style.display = "none";
-tacoexp.style.display = "none";
+//audiexp.style.display = "none";
+//viscoexp.style.display = "none";
+//tacoexp.style.display = "none";
 
 if (apo > vpo && apo > tpo) {
   typeshere.innerHTML = "Auditory";
   //console.group("audi");
-  audiexp.style.display = "block";
+  //audiexp.style.display = "block";
   //document.querySelector(".wordshere").innerHTML = "bitch you are audi";
 } else if (vpo > apo && vpo > tpo) {
   typeshere.innerHTML = "Visual";
   //console.group("visu");
-  viscoexp.style.display = "block";
+  //viscoexp.style.display = "block";
   // document.querySelector(".wordshere").innerHTML = "bitch you are v";
 } else if (tpo > apo && tpo > vpo) {
   typeshere.innerHTML = "Tactile/Kinesthetic";
   //console.group("tac");
-  tacoexp.style.display = "block";
+  //tacoexp.style.display = "block";
   // document.querySelector(".wordshere").innerHTML = "bitch you are tacos";
 } else if (apo === vpo && apo > tpo) {
   typeshere.innerHTML = "Auditory or Visual";
   //audiexp.style.display = "block";
-  viscoexp.style.display = "block";
+  //viscoexp.style.display = "block";
 } else if (tpo === vpo && apo < tpo) {
   typeshere.innerHTML = "Tactile or Visual";
-  tacoexp.style.display = "block";
+  //tacoexp.style.display = "block";
   //viscoexp.style.display = "block";
 } else if (apo === tpo && tpo > vpo) {
   typeshere.innerHTML = "Auditory or Tactile";
-  audiexp.style.display = "block";
+  //audiexp.style.display = "block";
   // tacoexp.style.display = "block";
 }
 
